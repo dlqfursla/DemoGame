888f330e037eccafbbb2f9d630ccf1291e6fa1fa
Joowon <all_time_low@naver.com>
2021-08-11 18:16:30 +0900

    initial commit

--- DemoGame/src/Frame/CharacterPanel.java
+++ DemoGame/src/Frame/CharacterPanel.java
@@ -1,7 +1,6 @@
 package Frame;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 
--- DemoGame/src/Main/Main.java
+++ DemoGame/src/Main/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		FrameController frameController = new FrameController();
 		frameController.Start();
-		System.out.println("비쥬얼스튜디오에서 깃 사용하기 테스트");
 	}
 
 }
