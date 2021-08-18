8d1d61521d4e762d7c9c70e03be26cf24cd80fb5
Joowon <all_time_low@naver.com>
2021-08-19 00:16:59 +0900

    Update MapData

--- DemoGame/src/DataManager/MapData.java
+++ DemoGame/src/DataManager/MapData.java
@@ -3,15 +3,28 @@ package DataManager;
 import java.util.ArrayList;
 
 public class MapData {
-	ArrayList<int[]>[] LabelsMappingList;
-	ArrayList<int[]> map1LabelsMapping;
-	ArrayList<int[]> map2LabelsMapping;
-	ArrayList<int[]> map3LabelsMapping;
-	ArrayList<int[]> map4LabelsMapping;
-	ArrayList<int[]> map5LabelsMapping;
-	ArrayList<int[]> map6LabelsMapping;
+	ArrayList<ArrayList<Integer>> LabelsMappingList;
+	ArrayList<Integer> map1LabelsMapping;
+	ArrayList<Integer> map2LabelsMapping;
+	ArrayList<Integer> map3LabelsMapping;
+	ArrayList<Integer> map4LabelsMapping;
+	ArrayList<Integer> map5LabelsMapping;
+	ArrayList<Integer> map6LabelsMapping;
 
 	public MapData() {
 		
+	}
+	public void initMapData() {
+		//LabelsMapping = new ArrayList<int[]>[6];
+		map1LabelsMapping = new ArrayList<Integer>();
+		map2LabelsMapping = new ArrayList<Integer>();
+		map3LabelsMapping = new ArrayList<Integer>();
+		map4LabelsMapping = new ArrayList<Integer>();
+		map5LabelsMapping = new ArrayList<Integer>();
+		map6LabelsMapping = new ArrayList<Integer>();
+	}
+
+	public ArrayList<Integer> getmap1LabelsMapping() {
+		return map1LabelsMapping;
 	}
 }
--- DemoGame/src/Main/FrameController.java
+++ DemoGame/src/Main/FrameController.java
@@ -65,6 +65,7 @@ public class FrameController extends JFrame {
 		//setToBattlePanel();
 		//setToCharacterPanel();
 		//setToMapPanel();
+		//setToStatsPanel();
 
 	}
 
