5c2bc2825612789fad63245af9451f612756e71a
Joowon <all_time_low@naver.com>
2021-08-21 00:56:09 +0900

    Updata Skill

--- DemoGame/src/Data/Skill.java
+++ DemoGame/src/Data/Skill.java
@@ -5,10 +5,10 @@ public class Skill {
     int MaxGauge; // 채울 수 있는 총 게이지
     int Gauge; // 단위 게이지, MaxGauge를 몇 등분할지의 값
 
-    //디버프, 값에 따라 몇 턴 동안 지속될지 결정
-    int Stun;
-    int Weaken;
-    int Slow;
+    // 디버프, 값에 따라 몇 턴 동안 지속될지 결정
+    int Stun; // 다음 턴 쉼
+    int Weaken; // 가하는 데미지 30% 감소
+    int Slow; // 맞는 데미지 30% 증가
 
     public Skill(String Name, int MaxGauge, int Gauge) {
         this.Name = Name;
@@ -28,6 +28,18 @@ public class Skill {
         return Gauge;
     }
 
+    public int getStun() {
+        return Stun;
+    }
+
+    public int getWeaken() {
+        return Stun;
+    }
+
+    public int getSLow() {
+        return Stun;
+    }
+
     public void setName(String Name) {
         this.Name = Name;
     }
@@ -39,5 +51,17 @@ public class Skill {
     public void setGauge(int Gauge) {
         this.Gauge = Gauge;
     }
-    
+
+    public void setStun(int Stun) {
+        this.Stun = Stun;
+    }
+
+    public void setWeaken(int Weaken) {
+        this.Weaken = Weaken;
+    }
+
+    public void setSLow(int Slow) {
+        this.Slow = Slow;
+    }
+
 }
